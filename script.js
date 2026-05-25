2344e08eb140d4adb8b8d3841016f5963c792438
layonfreitas <lyon.rub@gmail.com>
2026-05-25 20:04:16 -0300

    atu

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
-var bolax = canvas.width / 2;
+var bolax = canvas.width * 0.5;
 var bolay = 53;
 var velocidadex = 0;
 var velocidadey = 0;
@@ -13,10 +13,6 @@ var inclinacao2 = 0;
 
 var xi1, yi1, xf1, yf1;
 var xi2, yi2, xf2, yf2;
-
-
-
-
 
 
 function animate(){
@@ -30,12 +26,20 @@ function animate(){
     c.fillRect(0, 0, canvas.width, 50);
 
     barras();
-    let yBarra = yi1 + inclinacao1 * (bolax - xi1);
 
-if( bolax > xi1 && bolax < xf1 && bolay + 10 > yBarra && bolay < yBarra){
-    bolay = yBarra - 10;
-    velocidadey *= -0.7;
-}
+    let yBarra = yi1 + inclinacao1 * (bolax - xi1);
+    if( bolax > xi1 && bolax < xf1 && bolay + 10 > yBarra && bolay < yBarra){
+        bolay = yBarra - 10;
+        velocidadey *= -0.7;
+        velocidadex += inclinacao1 * 0.4;
+    }
+
+    let yBarra2 = yi2 + inclinacao2 * (bolax - xi2);
+    if (bolax > xi2 && bolax < xf2 && bolay + 10 > yBarra2 && bolay - 10 < yBarra2) {
+        bolay = yBarra2 - 10;
+        velocidadey *= -0.7;
+        velocidadex += inclinacao2 * 0.4;
+    }
 
     c.beginPath();
     c.arc(bolax, bolay, 10, 0, Math.PI * 2, false);
@@ -44,55 +48,59 @@ if( bolax > xi1 && bolax < xf1 && bolay + 10 > yBarra && bolay < yBarra){
 
     velocidadey += 0.5;
     bolay += velocidadey;
+    bolax += velocidadex;
 
     if (bolay + 10 > canvas.height){
         bolay = canvas.height - 10;
         velocidadey *= -0.5;
+         barra1 = false;
+         barra2 = false;
+         if (velocidadex <= 0){
+            velocidadex = 0;
+         }
+         else {
+            velocidadex -=1;
+         }
     }
 
+    if (bolax + 10 > canvas.width){
+        bolax = canvas.width - 10;
+        velocidadex *= -0;
+
+    }
+
+    if (bolax - 10 < 0){
+        bolax = 10;
+        velocidadex *= 0;
+    }
 }
 animate();
 
 function barras(){
-    function barras(){
 
     xi1 = canvas.width * 0.1;
     yi1 = canvas.height * 0.3;
-
     xf1 = canvas.width * 0.3;
     yf1 = canvas.height * 0.5;
 
-
-    xi2 = canvas.width * 0.7;
-    yi2 = canvas.height * 0.5;
-
-    xf2 = canvas.width * 0.9;
-    yf2 = canvas.height * 0.3;
-
-
+    xi2 = canvas.width * 0.3;
+    yi2 = canvas.height * 0.4;
+    xf2 = canvas.width * 0.7;
+    yf2 = canvas.height * 0.5;
     c.beginPath();
     c.moveTo(xi1, yi1);
     c.lineTo(xf1, yf1);
-
     c.lineWidth = 5;
     c.strokeStyle = "#ff0000";
     c.stroke();
 
-
     c.beginPath();
     c.moveTo(xi2, yi2);
     c.lineTo(xf2, yf2);
-
     c.lineWidth = 5;
     c.strokeStyle = "#00ff00";
     c.stroke();
 
-
     inclinacao1 = (yf1 - yi1) / (xf1 - xi1);
     inclinacao2 = (yf2 - yi2) / (xf2 - xi2);
-
-}
-       
-
-
 }
